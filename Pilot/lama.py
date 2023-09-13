e6a62a7cc1ddf521280ab26c7fbe3f8526cc698a
Dima <ddligach@luc.edu>
2023-09-13 10:49:01 -0500

    removed old code

--- Pilot/lama.py
+++ Pilot/lama.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python3
 
-import transformers, torch
+import transformers, torch, os
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from time import time
 
-model_path = "Models/Llama-2-70b-chat-hf"
-system_prompt = ''
+os.environ['CUDA_VISIBLE_DEVICES'] = '0'
+
+model_path = '/home/dima/Lama/Models/Llama-2-7b-chat-hf'
+system_prompt = 'You are a helpful AI assistant. You always give precise and short answers.'
 
 def main():
   """Ask for input and feed into llama2"""
@@ -13,12 +15,10 @@ def main():
   start = time()
 
   tokenizer = AutoTokenizer.from_pretrained(model_path)
-
   model = AutoModelForCausalLM.from_pretrained(
-    model_path, 
+    model_path,
     device_map='auto',
-    load_in_4bit=True)
-
+    load_in_8bit=True)
   pipeline = transformers.pipeline(
     'text-generation',
     model=model,
@@ -27,12 +27,12 @@ def main():
     device_map='auto')
 
   end = time()
-  print(f'\n[model load time: {end-start} seconds]\n')
+  print(f'\n[model load time: {end - start} seconds]\n')
 
-  for _ in range(100):
-    user_message = input('please type something: ')
-    prompt = f'<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n\n{user_message} [/INST]'
-        
+  while(True):
+
+    user_message = input('Prompt: ')
+    prompt = f'<s>[INST] <<SYS>>\n{system_prompt}\n<</SYS>>\n\n\n{user_message} [/INST]\n\n'
     start = time()
 
     outputs = pipeline(
@@ -41,17 +41,16 @@ def main():
       top_k=10,
       num_return_sequences=1,
       eos_token_id=tokenizer.eos_token_id,
+      temperature=0.001,
       max_length=500)
 
     end = time()
-
     for output in outputs:
       print('\n[***** begin generated text *****]\n')
       print(output['generated_text'])
-      print('\n[***** End generated text *****]\n')
+      print('\n[***** end generated text *****]\n')
 
-    print(f'[inference time: {end-start} seconds]\n')
-
+    print(f'[inference time: {end - start} seconds]\n')
 if __name__ == "__main__":
    
   main()
